f0a115d83bb1685db01bd14c588545453fb1fbe3
Evil0ctal <20760448+Evil0ctal@users.noreply.github.com>
2021-06-20 00:50:51 -0700

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -14,24 +14,24 @@ const c: IConfig = {
   showGithub: true,
 
   // 您的网站地址，有利于SEO
-  homeUrl: 'https://nav3.cn',
+  homeUrl: 'https://mycyberpunk.com',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: '波霸猫导航 - 精选实用导航网站',
 
   // 网站描述
-  description: '发现导航 - 精选实用导航网站',
+  description: '波霸猫导航 - 精选实用导航网站',
 
   // 网站关键字
-  keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
+  keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js,波霸猫,我的世界,Minecraft,服务器',
 
   // 默认主题: Light | Sim | Side | App | Shortcut
-  theme: 'Light',
+  theme: 'Sim',
 
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
     <div style="font-weight: bold;">共收录\${total}个网站</div>
-    <div>Copyright © 2018-2021 nav3.cn, All Rights Reserved</div>
+    <div>Copyright © 2019-2021 Boba.cat, All Rights Reserved</div>
   `,
 
   // 阿里巴巴矢量图库 https://www.iconfont.cn/
@@ -50,7 +50,7 @@ const c: IConfig = {
   simThemeConfig: {
     // 海报图
     posterImageUrls: [
-      'https://raw.sevencdn.com/xjh22222228/nav/image/sim-wallpaper.jpg'
+      'https://boba.cat/wp-content/uploads/2021/05/bilibili.png'
     ],
     description: '这里收录多达 <b>${total}</b> 个优质网站， 助您工作、学习和生活'
   }
